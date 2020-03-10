db070df32f8861867d8b1f83fe251a5ac2cd78fb
usama147 <gierdienusama@gmail.com>
2020-03-10 10:16:50 +0200

    Doing data types.

---
+++ 10March.py
@@ -0,0 +1,17 @@
+x = "Just sitting in class."
+print(type(x))  # Displaying data type of x.
+print(x[2:5])
+z = 5  # Declaring z as a variable 5.
+y = 5.8  # Declaring y as a variable 5.8.
+print(type(z))  # Prints data type of variable z.
+print(type(y))  # Prints data type of variable y.
+mylist = [23, 82, 42, "Andrew"]  # Declaring a list.
+print(type(mylist))  # Prints data type of "mylist".
+mylist.insert(2, 54)  # Replaces te second variable with 54.
+print(mylist)  # Prints "mylist".
+mylist.reverse()  # Reverses list.
+print(mylist) # Prints list.
+cars = {"C63 AMG", "Aston Martin", "Bugati"}  # Creates list.
+print(type(cars)) # Prints data type of "cars".
+cars.add("Porche") # Adds to list.
+print(cars) # Prints "cars" list.
